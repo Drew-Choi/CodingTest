47e7b297bb716f3e322fc82cb23a3578e115b9e3
Drew <drc920630@gmail.com>
2023-02-17 00:31:52 +0900

    피자 나눠먹기 까지 완료

---
+++ CodingTest/src/lv_0/PizzaDivide.java
@@ -0,0 +1,25 @@
+package lv_0;
+
+//머쓱이네 피자가게는 피자를 일곱 조각으로 잘라 줍니다.
+//피자를 나눠먹을 사람의 수 n이 주어질 때, 
+//모든 사람이 피자를 한 조각 이상 먹기 위해 필요한 피자의 수를 
+//return 하는 solution 함수를 완성해보세요.
+
+public class PizzaDivide {
+	
+	public int solution(int n) {
+        int answer = 0;
+        int pizza = 0;
+        int addpizza = 0;
+        
+        pizza = (int) n / 7;
+        if (n % 7 != 0) {
+           addpizza += 1;
+        }
+        
+        answer = pizza + addpizza;
+        
+        return answer;
+    }
+
+}
